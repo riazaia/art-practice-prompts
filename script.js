c6fe39c861af0d4a85f499ad0eebb7b00d046dab
ria <42520601+riazaia@users.noreply.github.com>
2025-04-03 18:35:20 -0300

    Move prompts to their own file

--- script.js
+++ script.js
@@ -1,58 +1,26 @@
-let prompts = {
-  uno: [
-    "line weight",
-    "shape and form construction",
-    "composition",
-    "values and light",
-    "depth and perspective",
-  ],
-  dos: [
-    "materials and textures",
-    "clothes and drapery",
-    "scenery and background",
-    "human anatomy",
-    "animal anatomy",
-  ],
-  tres: ["character design", "environments"],
-};
+const PROMPTS_PATH = "data.json";
 
-function chooseRandomly() {
-  let arr = [];
-  Array.from(
-    document.querySelectorAll('input[type="checkbox"]:checked')
-  ).forEach((item) => {
-    arr = [...arr, ...prompts[item.getAttribute("id")]];
-  });
-
-  if (arr.length === 0) return alert("You must select an option!");
-
-  var randomNumber = Math.floor(Math.random() * arr.length);
-  document.getElementById("result").textContent = arr[randomNumber];
+async function loadJSON(path) {
+	try {
+		const response = await fetch(path);
+		const data = await response.json();
+		return data;
+	} catch (error) {
+		console.error("Error loading JSON:", error);
+	}
 }
 
-/////////// same function but using a ternary operator
-// function chooseRandomly() {
-//   let arr = [];
-//   arr = arr.concat(document.getElementById("uno").checked ? uno : []);
-//   arr = arr.concat(document.getElementById("dos").checked ? dos : []);
-//   arr = arr.concat(document.getElementById("tres").checked ? tres : []);
-//
-//   if (arr.length === 0) return alert("You must select an option!");
-//   var randomNumber = Math.floor(Math.random() * arr.length);
-//   document.getElementById("result").textContent = arr[randomNumber];
-// }
+async function chooseRandomly() {
+	let prompts = await loadJSON(PROMPTS_PATH);
+	let arr = [];
+	Array.from(
+		document.querySelectorAll('input[type="checkbox"]:checked')
+	).forEach((item) => {
+		arr = [...arr, ...prompts[item.getAttribute("id")]];
+	});
 
-////////////////// same function but using the Array.reduce()
-// function chooseRandomly() {
-//   let arr = [];
-//   arr = Array.from(document.querySelectorAll('input[type="checkbox"]')).reduce(
-//     (arr, item) => {
-//       if (!item.checked) return arr;
-//       return [...arr, ...prompts[item.getAttribute("id")]];
-//     },
-//     []
-//   );
-//   if (arr.length === 0) return alert("You must select an option!");
-//   var randomNumber = Math.floor(Math.random() * arr.length);
-//   document.getElementById("result").textContent = arr[randomNumber];
-// }
+	if (arr.length === 0) return alert("You must select an option!");
+
+	var randomNumber = Math.floor(Math.random() * arr.length);
+	document.getElementById("result").textContent = arr[randomNumber];
+}
